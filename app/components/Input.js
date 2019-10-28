3426c0f28da9dacc6406eb29d85a8210a74c6075
zhsks528 <hsm950825@naver.com>
2019-10-28 22:48:54 +0900

    Input 컴포넌트 생성

---
+++ app/components/Input.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+
+export default function Subtitle() {
+  return (
+    <TextInput
+      styles={styles.input}
+      placeholder={"해야할 일을 입력해주세요"}
+      maxLength={30}
+      returnKeyType="done"
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 17,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 20
+  }
+});
