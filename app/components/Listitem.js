5bf3d6cb9f77be75d659ccd5fd61c0baeaa0353a
zhsks528 <hsm950825@naver.com>
2019-10-28 22:49:09 +0900

    Listitem 컴포넌트 생성

---
+++ app/components/Listitem.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function Listitem({ name }) {
+  return (
+    <View style={styles.listitembox}>
+      <View style={styles.makerow}>
+        <TouchableOpacity>
+          <AntDesign name="checkcircle" size={20} style={styles.checkmargin} />
+        </TouchableOpacity>
+        <Text style={styles.item}>{name}</Text>
+      </View>
+      <TouchableOpacity>
+        <AntDesign name="close" size={20} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  listitembox: {
+    borderBottomWidth: 1,
+    padding: 5,
+    marginTop: 10,
+    width: width - 60,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  makerow: {
+    flexDirection: "row"
+  },
+  checkmargin: {
+    marginRight: 10
+  },
+  item: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
