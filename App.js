9109753373efd63154d6b72b05031c252b1837f7
zhsks528 <hsm950825@naver.com>
2019-10-28 22:49:30 +0900

    Listitem 컴포넌트 적용

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import Header from "./app/components/Header";
 import Subtitle from "./app/components/Subtitle";
 import Input from "./app/components/Input";
+import Listitem from "./app/components/Listitem";
 
 export default function App() {
   return (
@@ -16,6 +17,8 @@ export default function App() {
       </View>
       <View style={styles.subtitleposi}>
         <Subtitle title="해야 할 일 목록" />
+        <Listitem name="코딩하기" />
+        <Listitem name="운동하기" />
       </View>
     </View>
   );
